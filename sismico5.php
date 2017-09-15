4b503c7f608b27d38cb7e8250eb0e180c0e7df2b
irving negrete <irvingnegrete@MacBook-Pro-de-irving.local>
2017-09-14 23:58:14 -0500

    sismico5

---
+++ sismico5.php
@@ -0,0 +1,727 @@
+<?php $pagename = basename($_SERVER['PHP_SELF']); ?>
+<!DOCTYPE html>
+
+<!--[if IE 8]>			<html class="ie ie8"> <![endif]-->
+
+<!--[if IE 9]>			<html class="ie ie9"> <![endif]-->
+
+<!--[if gt IE 9]><!-->	<html lang="es"> <!--<![endif]-->
+
+	<head>
+
+		<meta charset="utf-8" />
+
+		<title>Geotem - Sismica de Reflexión</title>
+
+		<meta name="keywords" content="HTML5,CSS3,Template" />
+
+		<meta name="description" content="" />
+
+		<meta name="Author" content="Dorin Grigoras [www.stepofweb.com]" />
+
+
+
+		<!-- mobile settings -->
+
+		<meta name="viewport" content="width=device-width, maximum-scale=1, initial-scale=1, user-scalable=0" />
+
+		<!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
+
+
+
+		<!-- WEB FONTS : use %7C instead of | (pipe) -->
+
+		
+
+
+
+		<!-- CORE CSS -->
+
+		<link href="assets/plugins/bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css" />
+
+
+
+		<!-- REVOLUTION SLIDER -->
+
+		<link href="assets/plugins/slider.revolution.v5/css/pack.css" rel="stylesheet" type="text/css" />
+
+
+
+		<!-- THEME CSS -->
+
+		<link href="assets/css/essentials.css" rel="stylesheet" type="text/css" />
+
+		<link href="assets/css/layout.css" rel="stylesheet" type="text/css" />
+
+		<link href="assets/css/thematics-restaurant.css" rel="stylesheet" type="text/css" />
+
+
+
+		<!-- PAGE LEVEL SCRIPTS -->
+
+		<link href="assets/css/header-1.css" rel="stylesheet" type="text/css" />
+
+		<link href="assets/css/color_scheme/lightgrey.css" rel="stylesheet" type="text/css" id="color_scheme" />
+
+		<link href="assets/css/hover.css" rel="stylesheet" media="all">
+
+
+
+	</head>
+
+
+
+	<!--
+
+		AVAILABLE BODY CLASSES:
+
+		
+
+		smoothscroll 			= create a browser smooth scroll
+
+		enable-animation		= enable WOW animations
+
+
+
+		bg-grey					= grey background
+
+		grain-grey				= grey grain background
+
+		grain-blue				= blue grain background
+
+		grain-green				= green grain background
+
+		grain-blue				= blue grain background
+
+		grain-orange			= orange grain background
+
+		grain-yellow			= yellow grain background
+
+		
+
+		boxed 					= boxed layout
+
+		pattern1 ... patern11	= pattern background
+
+		menu-vertical-hide		= hidden, open on click
+
+		
+
+		BACKGROUND IMAGE [together with .boxed class]
+
+		data-background="assets/images/boxed_background/1.jpg"
+
+	-->
+
+	<body class="smoothscroll enable-animation">
+
+		<style>
+
+    		@font-face{
+
+				font-family: 'din';
+
+				src:url('assets/fonts/DINPro-CondMedium.otf');
+
+			}
+
+		</style>
+
+
+
+		<!-- wrapper -->
+
+		<div id="wrapper">
+
+
+			<?php include 'inc/header.php'; ?>
+
+
+
+
+
+			<section style="padding:0;">
+
+				<div class="container">
+
+					<div id="rev_slider_18_1_wrapper" class="rev_slider_wrapper fullwidthbanner-container" data-alias="highlight-showcase18" style="margin:0px auto;background-color:transparent;padding:0px;margin-top:0px;margin-bottom:0px;">
+
+						<!-- START REVOLUTION SLIDER 5.0.7 auto mode -->
+
+						<div id="rev_slider_18_1" class="rev_slider fullwidthabanner" style="display:none;" data-version="5.0.7">
+
+							<ul>
+
+
+
+
+
+								<!-- SLIDE 1 -->
+
+								<li data-index="rs-59" data-transition="slideoverhorizontal" data-slotamount="7"  data-easein="default" data-easeout="default" data-masterspeed="1500"  data-thumb="assets/images/demo/rs5/newspaper_bg1-100x50.jpg"  data-rotate="0"  data-fstransition="fade" data-fsmasterspeed="1000" data-fsslotamount="7" data-saveperformance="off"  data-title="Construcción" data-description="">
+
+									<!-- MAIN IMAGE -->
+
+									<img src="img/banner/bmetodos/SismReflex.JPG"  alt=""  data-bgposition="center bottom" data-bgfit="cover" data-bgrepeat="no-repeat" data-bgparallax="10" class="rev-slidebg" data-no-retina>
+
+									<div class="overlay dark-4"></div>
+
+									<!-- LAYERS -->
+
+
+
+									<!-- LAYER NR. 1 -->
+
+									<div class="tp-caption News-Title   tp-resizeme rs-parallaxlevel-0" 
+
+										id="slide-59-layer-1" 
+
+										data-x="['left','left','left','left']" data-hoffset="['1000','800','400','400']" 
+
+										data-y="['top','top','top','top']" data-voffset="['490','490','290','190']" 
+
+										data-width="15em"
+
+										data-height="133"
+
+										data-whitespace="normal"
+
+										data-transform_idle="o:1;"
+
+							 
+
+										data-transform_in="y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;s:1500;e:Power3.easeInOut;" 
+
+										data-transform_out="y:[100%];s:1000;s:1000;" 
+
+										data-mask_in="x:0px;y:0px;s:inherit;e:inherit;" 
+
+										data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;" 
+
+										data-start="500" 
+
+										data-splitin="none" 
+
+										data-splitout="none" 
+
+										data-responsive_offset="on" 
+
+
+
+										
+
+										style="z-index: 5; min-width: 464px; max-width: 464px; max-width: 233px; max-width: 233px; white-space: normal;">Aplicaciones
+
+									</div>
+
+
+
+									<!-- LAYER NR. ADQUIS-->
+
+									<div class="tp-caption   rs-parallaxlevel-0" 
+
+										id="slide-20-layer-2" 
+
+										data-x="['left','left','left','left']" data-hoffset="['1270','880','383','183']" 
+
+										data-y="['top','top','top','top']" data-voffset="['440','440','-40','-40']" 
+
+										data-width="none"
+
+										data-height="none"
+
+										data-whitespace="nowrap"
+
+										data-transform_idle="o:1;"
+
+							 
+
+										data-transform_in="x:[-100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;s:1500;e:Power3.easeInOut;" 
+
+										data-transform_out="x:[100%];s:1000;e:Power3.easeInOut;s:1000;e:Power3.easeInOut;" 
+
+										data-mask_in="x:0px;y:0px;s:inherit;e:inherit;" 
+
+										data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;" 
+
+										data-start="500" 
+
+										data-responsive_offset="on" 
+
+
+
+										
+
+										style="z-index: 6; zoom:0.7;"><a href="geotecnica.php"><img class="footer-logo" src="img/iconos/icongeotecnia.png"  width="50" height="auto"alt="geotermia" /></a>
+
+									</div>
+
+
+
+									<!-- LAYER NR. INTEGRA -->
+
+									<div class="tp-caption   rs-parallaxlevel-0" 
+
+										id="slide-20-layer-2" 
+
+										data-x="['left','left','left','left']" data-hoffset="['1390','1580','1540','1540']" 
+
+										data-y="['top','top','top','top']" data-voffset="['440','440','-40','-40']" 
+
+										data-width="none"
+
+										data-height="none"
+
+										data-whitespace="nowrap"
+
+										data-transform_idle="o:1;"
+
+							 
+
+										data-transform_in="x:[-100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;s:1500;e:Power3.easeInOut;" 
+
+										data-transform_out="x:[100%];s:1000;e:Power3.easeInOut;s:1000;e:Power3.easeInOut;" 
+
+										data-mask_in="x:0px;y:0px;s:inherit;e:inherit;" 
+
+										data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;" 
+
+										data-start="500" 
+
+										data-responsive_offset="on" 
+
+
+
+										
+
+										style="z-index: 6; zoom:0.47; zoom:0.7"><a href="medio-ambiente.php"><img class="footer-logo" src="img/iconos/icoambiental.png"  width="100" height="auto"alt="" /></a>
+
+									</div>
+
+
+
+									<!-- LAYER NR. ENTRENA -->
+
+									<div class="tp-caption   rs-parallaxlevel-0" 
+
+										id="slide-20-layer-2" 
+
+										data-x="['left','left','left','left']" data-hoffset="['1510','1580','1540','1540']" 
+
+										data-y="['top','top','top','top']" data-voffset="['440','440','-40','-40']" 
+
+										data-width="none"
+
+										data-height="none"
+
+										data-whitespace="nowrap"
+
+										data-transform_idle="o:1;"
+
+							 
+
+										data-transform_in="x:[-100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;s:1500;e:Power3.easeInOut;" 
+
+										data-transform_out="x:[100%];s:1000;e:Power3.easeInOut;s:1000;e:Power3.easeInOut;" 
+
+										data-mask_in="x:0px;y:0px;s:inherit;e:inherit;" 
+
+										data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;" 
+
+										data-start="500" 
+
+										data-responsive_offset="on" 
+
+
+
+										
+
+										  style="z-index: 6; zoom:0.7;"><a href="arqueologia.php"><img class="footer-logo" src="img/iconos/iconeduca.png"  width="100" height="auto"alt="" /></a>
+
+									</div>
+
+								</li>								
+
+							</ul>
+
+							
+
+							<div class="tp-bannertimer" style="height: 6px; background-color: rgba(46, 209, 255, 1.00);"></div>	
+
+						</div>
+
+					</div><!-- END REVOLUTION SLIDER -->
+
+
+
+				</div>
+
+			</section>
+
+
+
+
+
+
+
+			<!-- WELCOME -->
+
+			<section style="padding-top: 20px;">
+
+				<div class="container">
+
+
+
+					<div class="row">
+
+						<div style="margin-bottom: 30px;" class="heading-title heading-dotted text-center">
+
+							<img class="img-responsive" src="img/metodos/sismico5/titulo13.png" alt="">
+
+						</div>
+
+
+
+						<div class="col-sm-6">
+
+							<p class=" justify weight-400 letter-spacing-1 size-25">El método sísmico de refracción se desarrolló en la década de los 50´s y fue muy empleado en la industria petrolera antes de las innovaciones tecnológicas alcanzadas por los métodos de reflexión. <br> Sin embargo este método tiene grandes ventajas de aplicación, en sitios donde las discontinuidades o cambios de formación están muy próximos a la superficie.  La propiedad física a medir en los métodos sísmicos es la velocidad de propagación de las ondas que viajan a través del suelo. </p>
+
+							<hr style="margin-bottom: -20px;"/>
+
+							<div align="center"><img align="center" src="img/iconos/sismicos.png" alt="sismico"></div>
+
+							
+
+							<p class=" text-center weight-400 letter-spacing-1 size-30">Profundidad: 5 a 40 m <br>Fuente: Artificial <br>Modalidad Terrestre</p>
+
+						</div>
+
+
+
+						<div class="col-sm-6">
+
+							<div class="masonry-gallery columns-2 clearfix lightbox" data-img-big="1" data-plugin-options='{"delegate": "a", "gallery": {"enabled": true}}'>
+
+								<a class="image-hover" href="img/metodos/sismico5/SismReflex(1).jpg">
+
+									<img src="img/metodos/sismico5/SismReflex(1).jpg" alt="...">
+
+								</a> <br>
+							</div>
+
+
+							<div class="masonry-gallery columns-3 clearfix lightbox" data-img-big="" data-plugin-options='{"delegate": "a", "gallery": {"enabled": true}}'>
+
+
+								<a class="image-hover" href="img/metodos/sismico5/Etapa2a.jpg" >
+
+									<img src="img/metodos/sismico5/Etapa2a.jpg" alt="...">
+
+								</a>
+
+								<a class="image-hover" href="img/metodos/sismico5/Etapa2b.jpg" >
+
+									<img src="img/metodos/sismico5/Etapa2b.jpg" alt="...">
+
+								</a>
+
+							</div>
+						</div>
+					</div>
+
+				</div>
+
+			</section>
+
+			<!-- /WELCOME -->
+
+
+
+			<!--2 -->
+
+			<section class="alternate">
+
+				<div class="container">
+					<div class="row">
+
+						<div class="col-sm-6">
+
+							<div class="masonry-gallery columns-2 clearfix lightbox margin-top-30" data-img-big="1" data-plugin-options='{"delegate": "a", "gallery": {"enabled": true}}'>
+
+								<a class="image-hover" href="img/metodos/sismico5/Etapa3a.jpg">
+
+									<img src="img/metodos/sismico5/Etapa3a.jpg" alt="...">
+
+								</a> 
+
+							</div>
+
+						</div>
+
+
+						<div class="col-sm-6">
+
+							<p class=" justify weight-400 letter-spacing-1 size-28">El resultado final es una sección distancia-tiempo en la que se resaltan las diferentes estructuras en el subsuelo, que sirven de reflectores sísmicos.  Para obtener una sección confiable se tienen que identificar los tipos de ondas de interés del resto de las señales recuperadas con los geófonos.  Las reflexiones de las ondas de compresión se producen en las interfaces del subsuelo donde existe suficiente contraste en los valores de impedancia acústica. </p>
+
+							<hr />							
+
+						</div>
+
+					</div>
+
+				</div>
+			</section>
+
+			<!-- / -->
+
+
+
+			<!-- sec3 -->
+
+			<section style="padding-top: 20px;">
+
+				<div class="container">
+
+					<div class="row">
+
+						<div class="col-sm-6">
+
+							<p class=" justify weight-400 letter-spacing-1 size-25 margin-top-30">Durante el procesamiento se suman los registros que tienen ese punto reflector común y de esta manera se busca mejorar la señal/ruido.  Para garantizar una buena sección sísmica de reflexión, el diseño de adquisición es cuidadosamente planeado y los datos son seleccionados y filtrados.  La campaña para la adquisición de datos está en función de las características geológicas y/o el objetivo a resolver.  El número de canales, su espaciamiento, offset y tipo de fuente deben ser considerados para cada sitio a estudiar.<br> <br> <br><br> <br> <br>La sección debe ser interpretada sin ninguna ambigüedad, considerando las pertinentes interpretaciones geológicas.  Es muy importante la obtención de secciones distancia-profundidad a partir de la sección sísmica distancia-tiempo, debido a que el proceso de los datos puede generar reflexiones que no correspondan a la geología.  </p>
+
+							<hr style="margin-bottom: -20px;"/>
+
+						</div>
+
+
+						<div class="col-sm-6">
+
+
+							<div class="masonry-gallery columns-2 clearfix lightbox" data-img-big="1" data-plugin-options='{"delegate": "a", "gallery": {"enabled": true}}'>
+
+
+
+								<a class="image-hover" href="img/metodos/sismico5/SismReflex(0).JPG" >
+
+									<img src="img/metodos/sismico5/SismReflex(0).JPG" alt="...">
+
+								</a>
+
+							</div>
+
+							<div class="masonry-gallery columns-2 clearfix lightbox" data-img-big="1" data-plugin-options='{"delegate": "a", "gallery": {"enabled": true}}'>
+
+								<a class="image-hover" href="img/metodos/sismico5/Etapa3b.jpg">
+
+									<img src="img/metodos/sismico5/Etapa3b.jpg" alt="...">
+
+								</a> <br>
+							</div>
+
+						</div>
+					</div>
+
+				</div>
+
+			</section>
+
+			<!-- /sec3 -->
+
+
+
+			<!-- FOOTER -->
+
+			<?php include 'inc/footer.php'; ?>
+
+			<!-- /FOOTER -->
+
+
+
+
+
+		</div>
+
+		<!-- /wrapper -->
+
+
+
+
+
+		<!-- SCROLL TO TOP -->
+
+		<a href="#" id="toTop"></a>
+
+
+
+
+
+		<!-- PRELOADER -->
+
+		<div id="preloader">
+
+			<div class="inner">
+
+				<span class="loader"></span>
+
+			</div>
+
+		</div><!-- /PRELOADER -->
+
+
+
+		<script type="text/javascript"  src="assets/plugins/jquery/jquery-2.1.4.min.js"></script>
+
+		<script type="text/javascript" src="assets/js/scripts.js"></script>
+
+
+
+
+
+		<script type="text/javascript">
+
+			jQuery(document).ready(function() {
+
+				if(jQuery("#rev_slider_18_1").revolution == undefined){
+
+					revslider_showDoubleJqueryError("#rev_slider_18_1");
+
+				}else{
+
+					revapi18 = jQuery("#rev_slider_18_1").show().revolution({
+
+						sliderType:"standard",
+
+						jsFileLocation:plugin_path + "slider.revolution.v5/js/",
+
+						sliderLayout:"100%",
+
+						dottedOverlay:"none",
+
+						delay:10000,
+
+						navigation: {
+
+							keyboardNavigation:"off",
+
+							keyboard_direction: "horizontal",
+
+							mouseScrollNavigation:"off",
+
+							onHoverStop:"off",
+
+							touch:{
+
+								touchenabled:"on",
+
+								swipe_threshold: 75,
+
+								swipe_min_touches: 1,
+
+								swipe_direction: "horizontal",
+
+								drag_block_vertical: false
+
+							}
+
+							,
+
+						},
+
+						viewPort: {
+
+							enable:true,
+
+							outof:"pause",
+
+							visible_area:"80%"
+
+						},
+
+						responsiveLevels:[1240,1024,778,480],
+
+						gridwidth:[2000,1024,767,480],
+
+						gridheight:[620,620,380,260],
+
+						lazyType:"none",
+
+						parallax: {
+
+							type:"scroll",
+
+							origo:"enterpoint",
+
+							speed:400,
+
+							levels:[5,10,15,20,25,30,35,40,45,50],
+
+						},
+
+						shadow:0,
+
+						spinner:"off",
+
+						stopLoop:"off",
+
+						stopAfterLoops:-1,
+
+						stopAtSlide:-1,
+
+						shuffle:"off",
+
+						autoHeight:"off",
+
+						hideThumbsOnMobile:"off",
+
+						hideSliderAtLimit:0,
+
+						hideCaptionAtLimit:0,
+
+						hideAllCaptionAtLilmit:0,
+
+						debugMode:false,
+
+						fallbacks: {
+
+							simplifyAll:"off",
+
+							nextSlideOnWindowFocus:"off",
+
+							disableFocusListener:false,
+
+						}
+
+					});
+
+				}
+
+			});
+
+		</script>
+
+		<script type="text/javascript" src="assets/plugins/slider.revolution.v5/js/extensions/revolution.extension.slideanims.min.js"></script>
+
+		<script type="text/javascript" src="assets/plugins/slider.revolution.v5/js/extensions/revolution.extension.layeranimation.min.js"></script>
+
+		<script type="text/javascript" src="assets/plugins/slider.revolution.v5/js/extensions/revolution.extension.navigation.min.js"></script>
+
+		<script type="text/javascript" src="assets/plugins/slider.revolution.v5/js/extensions/revolution.extension.parallax.min.js"></script>
+
+		<!-- JAVASCRIPT FILES -->
+
+		<script type="text/javascript">var plugin_path = 'assets/plugins/';</script>
+
+	
+
+
+
+		<!-- REVOLUTION SLIDER -->
+
+		<script type="text/javascript" src="assets/plugins/slider.revolution.v5/js/jquery.themepunch.tools.min.js"></script>
+
+		<script type="text/javascript" src="assets/plugins/slider.revolution.v5/js/jquery.themepunch.revolution.min.js"></script>
+
+
+
+	</body>
+
+</html>
